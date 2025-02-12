8c735884ff5a561972212ef2f592a8b7aee6e31a
EYA <mejrieya384@gmail.com>
2025-02-12 20:47:18 +0100

    second commit

--- src/main/java/tn/esprit/models/User.java
+++ src/main/java/tn/esprit/models/User.java
@@ -1,50 +1,107 @@
 package tn.esprit.models;
 
+import java.sql.Date;
+import tn.esprit.models.Role;
+
+
 public class User {
-    int id,age;
-    String name, lastname;
+    private int id_user ;
+    private String nom, prenom,email,mot_de_passe;
+    private int telephonne ;
+    private String photo_profil; // Nouveau champ
+    private Role role; // Nouveau champ ("conducteur" ou "passager")
+
+
 
     public User(){
     }
-    public User( String name, String lastname,int age) {
-        this.name = name;
-        this.lastname = lastname;
-        this.age = age;
+    public User( int id_user , String nom, String prenom,String email , String mot_de_passe, int telephonne ,  String photo_profil , Role role  ) {
+        this.id_user = id_user ;
+        this.nom = nom ;
+        this.prenom = prenom ;
+        this.email = email ;
+        this.mot_de_passe = mot_de_passe ;
+        this.telephonne = telephonne ;
+        this.photo_profil=photo_profil;
+        this.role=role;
     }
 
-    public int getId(){
-        return id;
+    public int getId_user() {
+        return id_user;
     }
-    public void setId(int id){
-        this.id = id;
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
     }
-    public String getName(){
-        return name;
+
+    public String getNom() {
+        return nom;
     }
-    public void setName(String name){
-        this.name = name;
+
+    public void setNom(String nom) {
+        this.nom = nom;
     }
-    public String getLastname(){
-        return lastname;
+
+    public String getPrenom() {
+        return prenom;
     }
-    public void setLastname(String lastname){
-        this.lastname = lastname;
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
     }
-    public int getAge(){
-        return age;
+
+    public String getEmail() {
+        return email;
     }
-    public void setAge(int age){
-        this.age = age;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMot_de_passe() {
+        return mot_de_passe;
+    }
+
+    public void setMot_de_passe(String mot_de_passe) {
+        this.mot_de_passe = mot_de_passe;
+    }
+
+    public int getTelephonne() {
+        return telephonne;
+    }
+
+    public void setTelephonne(int telephonne) {
+        this.telephonne = telephonne;
+    }
+
+    public String getPhoto_profil() {
+        return photo_profil;
+    }
+
+    public void setPhoto_profil(String photo_profil) {
+        this.photo_profil = photo_profil;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     @Override
-    public String toString(){
-        return "User{"+
-                "id= "+id+ '\''+
-                ", Name= "+ name+ '\''+
-                ", lastname= "+lastname+ '\''+
-                ", age= "+age + '\''+
-                "}\n";
+    public String toString() {
+        return "User{" +
+                "id_user=" + id_user +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", mot_de_passe='" + mot_de_passe + '\'' +
+                ", telephonne=" + telephonne +
+                ", photo_profil='" + photo_profil + '\'' +
+                ", role='" + role + '\'' +
+                '}';
     }
 }
 
--- src/main/java/tn/esprit/services/ServiceUser.java
+++ src/main/java/tn/esprit/services/ServiceUser.java
@@ -3,6 +3,7 @@ package tn.esprit.services;
 import tn.esprit.interfaces.IService;
 import tn.esprit.models.User;
 import tn.esprit.util.MyDataBase;
+import tn.esprit.models.Role;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -13,14 +14,20 @@ public class ServiceUser implements IService<User> {
     public ServiceUser() {
         cnx = MyDataBase.getInstance().getCnx();
     }
+
     @Override
     public void add(User user) {
-        String query = "INSERT INTO `user`(`nom`, `prenom`, `age`) VALUES ("+user.getName()+","+user.getLastname()+","+user.getAge()+")";
+        String query = "INSERT INTO `user`(`nom`, `prenom`, `email`, `mot_de_passe`, `telephonne` , `photo_profil`, `role`) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             PreparedStatement pstm = cnx.prepareStatement(query);
-            pstm.setString(1, user.getName());
-            pstm.setString(2, user.getLastname());
-            pstm.setInt(3, user.getAge());
+            pstm.setString(1, user.getNom());
+            pstm.setString(2, user.getPrenom());
+            pstm.setString(3, user.getEmail());
+            pstm.setString(4, user.getMot_de_passe());
+            pstm.setInt(5, user.getTelephonne());
+            pstm.setString(6, user.getPhoto_profil());
+            pstm.setString(7, user.getRole().name());
+
             pstm.executeUpdate();
         }catch (SQLException e){
             System.out.println(e.getMessage());
@@ -36,10 +43,15 @@ public class ServiceUser implements IService<User> {
             ResultSet  rs =stm.executeQuery(qry);
             while (rs.next()){
                 User u = new User();
-                u.setId(rs.getInt("id"));
-                u.setName(rs.getString("nom"));
-                u.setLastname(rs.getString("prenom"));
-                u.setAge(rs.getInt("age"));
+                u.setId_user(rs.getInt("id_user"));
+                u.setNom(rs.getString("nom"));
+                u.setPrenom(rs.getString("prenom"));
+                u.setEmail(rs.getString("email"));
+                u.setMot_de_passe(rs.getString("mot_de_passe"));
+                u.setTelephonne(rs.getInt("telephonne"));
+                u.setPhoto_profil(rs.getString("photo_profil")); // Correction du setter
+                u.setRole(Role.fromString(rs.getString("role"))); // Correction de rs.geString()
+
                 users.add(u);
             }
         } catch (SQLException e) {
@@ -51,12 +63,79 @@ public class ServiceUser implements IService<User> {
     }
 
     @Override
-    public void update(User user) {
+    public User getById(int id_user) {
+        String query = "SELECT * FROM user WHERE id_user = ?";
+        User user = null;
 
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query);
+            pstm.setInt(1, id_user);
+            ResultSet rs = pstm.executeQuery();
+
+            if (rs.next()) {
+
+                user = new User(
+                        rs.getInt("id_user"),
+                        rs.getString("nom"),
+                        rs.getString("prenom"),
+                        rs.getString("email"),
+                        rs.getString("mot_de_passe"),
+                        rs.getInt("telephonne"),
+                        rs.getString("photo_profil"),
+                        Role.fromString(rs.getString("role"))
+
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la récupération de l'utilisateur : " + e.getMessage());
+        }
+        return user;
+    }
+
+
+    @Override
+    public void update(User user) {
+        String query = "UPDATE user SET nom = ?, prenom = ?, email = ?, mot_de_passe = ?, telephonne = ? ,photo_profil = ? , role = ? WHERE id_user = ?";
+
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query);
+            pstm.setString(1, user.getNom());
+            pstm.setString(2, user.getPrenom());
+            pstm.setString(3, user.getEmail());
+            pstm.setString(4, user.getMot_de_passe());
+            pstm.setInt(5, user.getTelephonne());
+            pstm.setString(6, user.getPhoto_profil());
+            pstm.setString(7, user.getRole().name());
+            pstm.setInt(6, user.getId_user()); // Condition WHERE
+
+            int rowsUpdated = pstm.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("L'utilisateur avec ID " + user.getId_user() + " a été mis à jour avec succès.");
+            } else {
+                System.out.println("Aucun utilisateur trouvé avec l'ID " + user.getId_user());
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la mise à jour de l'utilisateur : " + e.getMessage());
+        }
     }
 
     @Override
     public void delete(User user) {
+        String query = "DELETE FROM user WHERE id_user = ?";
 
+        try {
+            PreparedStatement pstm = cnx.prepareStatement(query);
+            pstm.setInt(1, user.getId_user()); // Suppression basée sur l'ID de l'utilisateur
+
+            int rowsDeleted = pstm.executeUpdate();
+            if (rowsDeleted > 0) {
+                System.out.println("L'utilisateur avec ID " + user.getId_user() + " a été supprimé avec succès.");
+            } else {
+                System.out.println("Aucun utilisateur trouvé avec l'ID " + user.getId_user());
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppression de l'utilisateur : " + e.getMessage());
+        }
     }
+
 }
