8e81d93783f0c2fd3c8db929e8d033f45ef0964e
EYA <mejrieya384@gmail.com>
2025-02-19 15:39:59 +0100

    5th commit

---
+++ src/main/java/tn/esprit/controllers/ResetPassController.java
@@ -0,0 +1,21 @@
+package tn.esprit.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ResetPassController {
+
+    @FXML
+    private TextField emailfield;
+
+    @FXML
+    private Button envoyercode;
+
+    @FXML
+    void EnvoyerCode(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/tn/esprit/controllers/UserInterfaceController.java
@@ -0,0 +1,21 @@
+package tn.esprit.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class UserInterfaceController {
+
+    @FXML
+    private Text creercompte;
+
+    @FXML
+    private Button seconnecter;
+
+    @FXML
+    void directionpageconnexion(ActionEvent event) {
+
+    }
+
+}
