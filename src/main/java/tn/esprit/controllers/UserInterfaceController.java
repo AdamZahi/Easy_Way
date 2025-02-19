eae88cd10ca49521ead188654ce0704eed1e1657
EYA <mejrieya384@gmail.com>
2025-02-19 16:59:42 +0100

    6th commit

--- src/main/java/tn/esprit/controllers/UserInterfaceController.java
+++ src/main/java/tn/esprit/controllers/UserInterfaceController.java
@@ -2,8 +2,16 @@ package tn.esprit.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserInterfaceController {
 
@@ -13,9 +21,36 @@ public class UserInterfaceController {
     @FXML
     private Button seconnecter;
 
-    @FXML
-    void directionpageconnexion(ActionEvent event) {
 
+    @FXML
+    void RedirectToSignIn(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/SignIn.fxml"));
+            Parent root = loader.load();
+
+            // Obtenir la scène actuelle et la remplacer
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    @FXML
+    void RedirectToSignUp(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/SignUp.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
 }
--- src/main/java/tn/esprit/test/MainFx.java
+++ src/main/java/tn/esprit/test/MainFx.java
@@ -15,7 +15,7 @@ public class MainFx extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SignIn.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/UserSpace.fxml"));
         try {
             Parent root =loader.load();
             Scene scene = new Scene(root);
