64f28738c159afc410b30be5f9aa4f2d1a3898cd
EYA <mejrieya384@gmail.com>
2025-02-21 14:27:21 +0100

    Hashage du mot de passe

--- src/main/java/tn/esprit/test/MainFx.java
+++ src/main/java/tn/esprit/test/MainFx.java
@@ -1,5 +1,6 @@
 package tn.esprit.test;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,7 +15,7 @@ public class MainFx extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader= new FXMLLoader(getClass().getResource("/VerifyCode.fxml"));
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/SignUp.fxml"));
         try {
             Parent root =loader.load();
             stage.setScene(new Scene(root));
