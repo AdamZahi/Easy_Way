3a3ec8007cbd94b5fba9c67ed9cedb45c0288efc
Adam zahi <zahi.adem10@gmail.com>
2025-03-01 13:55:36 +0100

    update main and mainFx

--- src/main/java/tn/esprit/test/Main.java
+++ src/main/java/tn/esprit/test/Main.java
@@ -1,31 +1,32 @@
 package tn.esprit.test;
 
+import com.vonage.client.VonageClient;
+import com.vonage.client.sms.SmsSubmissionResponse;
+import com.vonage.client.sms.SmsSubmissionResponseMessage;
+import com.vonage.client.sms.messages.TextMessage;
 import tn.esprit.services.ServiceConducteur ;
 
 
 import tn.esprit.services.event.ServiceEvenement;
+import tn.esprit.services.event.ServiceTwilio;
 
 
 public class Main {
     public static void main(String[] args) {
-         ServiceConducteur sc = new ServiceConducteur();
-        ServiceEvenement se = new ServiceEvenement();
+        VonageClient client = VonageClient.builder().apiKey("c8c34ce3")
+                .apiSecret("FChHt3T5SB9XvulI")
+                .build();
 
 
+        TextMessage message = new TextMessage("Easy Way",
+                "+21694103115",  // Replace with the recipient's number
+                "🚨 just adam ytesti f api SMS!");
 
-        //test Conducteur
-//        sc.add(new Conducteur("foulen","feltani","exemple@gmail.com","123456",5566332
-//                ,"https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/476973258_479698575190094_7338888870042912014_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Pp-S5DxMB6QQ7kNvgEKM2Jr&_nc_oc=Adhd_oxVUY92NYtQRlxEJuEhDwnGxgC-UERiarqQqg-AW94Q-U9VeC33G6oJ7pq-N98&_nc_zt=23&_nc_ht=scontent.ftun9-1.fna&_nc_gid=A9soiCDa2_K54-OGoc4MCHe&oh=00_AYB7GVFjptoWsQueX5wBlz3EQpjI45NWz8Z9iQdhXTLv8A&oe=67B2CE8E"
-//        ,"permis1526","3ans experience "));
-        //Conducteur updatedCon = new Conducteur(22,"foulen","flani","abcd@email.tn","123np",94103115,"picture here nigga","0000","10snin");
-//        sc.update(updatedCon);
-        //sc.delete(updatedCon);
-        System.out.println(se.getAll());
+        SmsSubmissionResponse response = client.getSmsClient().submitMessage(message);
 
-
-
-
-
+        for (SmsSubmissionResponseMessage messageResponse : response.getMessages()) {
+            System.out.println("📩 SMS Sent! Status: " + messageResponse.getStatus());
+        }
     }
 
 }
