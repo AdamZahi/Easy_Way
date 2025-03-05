8d7beba8cc53a1e2a9cc25f484f86c44ebc5883e
TayssirBennejma1 <bennejma.tayssir@esprit.tn>
2025-03-05 20:49:21 +0100

    admin role

--- src/main/java/tn/esprit/models/user/User.java
+++ src/main/java/tn/esprit/models/user/User.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class User {
     public enum Role {
-        Conducteur , Passager
+        Conducteur , Passager , Admin
     }
 
     private int id_user;
