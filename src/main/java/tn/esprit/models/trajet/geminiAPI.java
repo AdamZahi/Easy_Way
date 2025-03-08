e3faa22ea88ac3c3158d99a8c25f2f225f90eba5
Mzoughi-007 <chirazmzoughi2002@gmail.com>
2025-03-08 15:40:53 +0100

    Update geminiAPI.java

--- src/main/java/tn/esprit/models/trajet/geminiAPI.java
+++ src/main/java/tn/esprit/models/trajet/geminiAPI.java
@@ -12,7 +12,7 @@ import com.google.gson.JsonParser;
 public class geminiAPI {
 
     // Replace with your actual Gemini API key
-    private static final String API_KEY = "AIzaSyCLysb9iKCZI9FdVA9JtcZEoaMaoakFDcU";
+    private static final String API_KEY = "secret";
     private static final String API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY;
 
     // Context: Only answering questions related to transport prices for the EasyWay app
