09afd53e12da1d66ebd751998078780d3673f072
EYA <mejrieya384@gmail.com>
2025-05-11 15:02:22 +0100

    User updates

--- src/main/java/tn/esprit/test/Main.java
+++ src/main/java/tn/esprit/test/Main.java
@@ -1,20 +1,21 @@
-//import tn.esprit.models.user.Admin;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        // Créer un objet Admin avec des données fictives
-//        Admin admin = new Admin();
-//        admin.setNom("Doe");
-//        admin.setPrenom("Jane");
-//        admin.setEmail("jane.doe@example.com");
-//        admin.setMot_de_passe("admin123");
-//        admin.setTelephonne(12345678);
-//        admin.setPhoto_profil("jane.png"); // tu peux aussi mettre null
-//
-//        // Créer une instance de ton service qui contient la méthode add
-//        ServiceAdmin Serviceadmin = new ServiceAdmin(); // suppose que ta classe s'appelle comme ça
-//
-//        // Appeler la méthode d'ajout
-//        Serviceadmin.add(admin);
-//    }
-//}
+import tn.esprit.models.user.Admin;
+import tn.esprit.services.user.ServiceAdmin;
+
+public class Main {
+    public static void main(String[] args) {
+        // Créer un objet Admin avec des données fictives
+        Admin admin = new Admin();
+        admin.setNom("Doe");
+        admin.setPrenom("Jane");
+        admin.setEmail("jane.doe@example.com");
+        admin.setMot_de_passe("admin123");
+        admin.setTelephonne(12345678);
+        admin.setPhoto_profil("jane.png"); // tu peux aussi mettre null
+
+        // Créer une instance de ton service qui contient la méthode add
+        ServiceAdmin Serviceadmin = new ServiceAdmin(); // suppose que ta classe s'appelle comme ça
+
+        // Appeler la méthode d'ajout
+        Serviceadmin.add(admin);
+    }
+}
